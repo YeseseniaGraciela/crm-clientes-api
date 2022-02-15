e336cd94457a57d9015a5f7715befebcf81a57e3
Graciela Castro <castroyessenia02@gmail.com>
2022-02-15 00:44:21 -0500

    Changed uir

--- src/database/DBConnection.js
+++ src/database/DBConnection.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 
-const uri = "mongodb://localhost:27017/crm-clientes";
+const uri = "mongodb+srv://yessenia02:Negra0210.@curso-mongodb.npkhg.mongodb.net/crm-clientes";
 const DBConnection = async () => {
   try {
     await mongoose.connect(uri);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error(error);
+    throw new Error("Failed to initialize database")
   }
 };
 
